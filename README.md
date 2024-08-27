cdd567964b3fc24a46c1e3dc5339708474b1f8c6
erotufelixpascal <38206275+erotufelixpascal@users.noreply.github.com>
2024-08-27 10:24:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# starters
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/erotufelixpascal/starters)
---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { Component, signal } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    <h1>Hello from {{ name }}!</h1>
+    <a target="_blank" href="https://angular.dev/overview">
+      Learn more about Angular
+    </a>
+  `,
+})
+export class App {
+  name = 'Angular';
+  qauntity = signal(1)
+constructor(){
+  console.log('is quantity', this.qauntity())
+}
+}
+
+bootstrapApplication(App);
